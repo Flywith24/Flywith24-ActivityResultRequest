15bfacf6a7d02e0467d7c0edd3813c83c5dd98f4
fly_with24 <fly_with24@163.com>
2020-07-13 20:23:27 +0800

    take video

--- app/src/main/java/com/flywith24/request/MainActivity.kt
+++ app/src/main/java/com/flywith24/request/MainActivity.kt
@@ -1,27 +1,36 @@
 package com.flywith24.request
 
-import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.flywith24.activityresult.takePicture
 import com.flywith24.activityresult.takePicturePreview
+import com.flywith24.activityresult.takeVideo
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     /**
-     * 拍照
+     * 拍照，返回图片路径
      */
     fun takePictureClick(view: View) {
-        takePicture { path ->
+        takePicture(BuildConfig.APPLICATION_ID) { path ->
             Log.i(TAG, "take picture success path = $path")
         }
     }
 
-
+    /**
+     * 拍照，返回图片 bitmap
+     */
     fun takePicturePreviewClick(view: View) {
-        takePicturePreview { bitmap ->
+        takePicturePreview() { bitmap ->
             findViewById<ImageView>(R.id.image).setImageBitmap(bitmap)
+        }
+    }
+
+    fun takeVideoClick(view: View) {
+        takeVideo(BuildConfig.APPLICATION_ID) { path ->
+            Log.i(TAG, "take video success path = $path")
         }
     }
 
@@ -29,4 +38,5 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     companion object {
         private const val TAG = "MainActivity"
     }
+
 }
